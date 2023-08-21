106d02fef3e6b7ed2686a48fba23e280cafc7e13
Mihizz <nemanjasstepic@gmail.com>
2023-08-22 00:09:34 +0200

    Add project files.

---
+++ RegularPolyhedronCalculator/Polyhendron.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularPolyhedronCalculator
+{
+    abstract class Polyhendron
+    {
+        protected double edge;
+
+        protected Polyhendron(double edge)
+        {
+            this.edge = edge;
+        }
+
+        protected double Edge
+        {
+            get { return edge; }
+            set { edge = value; }
+        }
+
+        protected abstract double surfaceArea(double x);
+        protected abstract double volume(double x);
+        protected abstract double radiusCircumscribedSphere(double x);
+        protected abstract double radiusInscribedSphere(double x);
+        protected abstract double radiusMedium(double x);
+    }
+}
